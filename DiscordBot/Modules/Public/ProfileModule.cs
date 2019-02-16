d42e1b6251dcf1255e32579d50db35d5a17c1f72
Melissa <melissabrennan645@gmail.com>
2019-02-16 19:54:01 +0000

    Updated profile module to get and update data on the database

--- DiscordBot/Modules/Public/ProfileModule.cs
+++ DiscordBot/Modules/Public/ProfileModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Discord;
@@ -7,8 +8,11 @@ using Discord.WebSocket;
 
 using DiscordBot.Common;
 using DiscordBot.Common.Preconditions;
+using DiscordBot.Database;
 using DiscordBot.Extensions;
 using DiscordBot.Handlers;
+using DiscordBot.Objects;
+using MySql.Data.MySqlClient;
 
 namespace DiscordBot.Modules.Public
 {
@@ -48,28 +52,56 @@ namespace DiscordBot.Modules.Public
             [Command("name"), Summary("")]
             public async Task SetName([Remainder]string name)
             {
-                User.UpdateUser(Context.User.Id, name: name);
+                //User.UpdateUser(Context.User.Id, name: name);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@name", name)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET name=@name WHERE id='" + Context.User.Id + "';", queryParams);
+                
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the name on your profile to \"" + name + "\"!");
             }
     
             [Command("gender"), Summary("")]
             public async Task SetGender([Remainder]string gender)
             {
-                User.UpdateUser(Context.User.Id, gender: gender);
+                //User.UpdateUser(Context.User.Id, gender: gender);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@gender", gender)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET gender=@gender WHERE id='" + Context.User.Id + "';", queryParams);
+                
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the gender on your profile to \"" + gender + "\"!");
             }
     
             [Command("pronouns"), Summary("Set your pronouns!")]
             public async Task SetUserPronouns([Remainder]string pronouns)
             {
-                User.UpdateUser(Context.User.Id, pronouns: pronouns);
+                //User.UpdateUser(Context.User.Id, pronouns: pronouns);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@pronouns", pronouns)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET pronouns=@pronouns WHERE id='" + Context.User.Id + "';", queryParams);
+
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the pronouns on your profile to \"" + pronouns + "\"!");
             }
     
             [Command("about"), Summary("Set your about message!")]
             public async Task SetUserAbout([Remainder]string aboutMessage)
             {
-                User.UpdateUser(Context.User.Id, about: aboutMessage);
+                //User.UpdateUser(Context.User.Id, about: aboutMessage);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@aboutMessage", aboutMessage)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET about=@aboutMessage WHERE id='" + Context.User.Id + "';", queryParams);
+
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the about section on your profile!");
             }
     
@@ -84,7 +116,14 @@ namespace DiscordBot.Modules.Public
                 
                 if (Context.User.GetLevel() >= Configuration.Load().PrefixLevelRequirement)
                 {
-                    User.UpdateUser(Context.User.Id, customPrefix: prefix);
+                    //User.UpdateUser(Context.User.Id, customPrefix: prefix);
+                
+                    List<(string, string)> queryParams = new List<(string, string)>()
+                    {
+                        ("@customPrefix", prefix)
+                    };
+                    DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET customPrefix=@customPrefix WHERE id='" + Context.User.Id + "';", queryParams);
+
                     await ReplyAsync(Context.User.Mention + ", you have set `" + prefix + "` as a custom prefix for yourself. Please do take note that the following prefixes will work for you:\n```KEY: [Prefix][Command]\n" + prefix + " - User Set Prefix\n" + GuildConfiguration.Load(Context.Guild.Id).Prefix + " - Guild Set Prefix\n@" + DiscordBot.Bot.CurrentUser.Username + " - Global Prefix```");
                 }
                 else
@@ -120,9 +159,17 @@ namespace DiscordBot.Modules.Public
                         return;
                     }
     
-                    User.UpdateUser(Context.User.Id, aboutR: rValue);
-                    User.UpdateUser(Context.User.Id, aboutG: gValue);
-                    User.UpdateUser(Context.User.Id, aboutB: bValue);
+                    //User.UpdateUser(Context.User.Id, aboutR: rValue);
+                    //User.UpdateUser(Context.User.Id, aboutG: gValue);
+                    //User.UpdateUser(Context.User.Id, aboutB: bValue);
+                
+                    List<(string, string)> queryParams = new List<(string, string)>()
+                    {
+                        ("@aboutR", rValue.ToString()),
+                        ("@aboutG", gValue.ToString()),
+                        ("@aboutB", bValue.ToString())
+                    };
+                    DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET aboutR=@aboutR, aboutG=@aboutG, aboutB=@aboutB WHERE id='" + Context.User.Id + "';", queryParams);
     
                     Color aboutColor = new Color(User.Load(Context.User.Id).AboutR, User.Load(Context.User.Id).AboutG, User.Load(Context.User.Id).AboutB);
     
@@ -143,42 +190,84 @@ namespace DiscordBot.Modules.Public
             [Alias("minecraftusername", "mcreg", "mcregister")]
             public async Task SetMinecraftUsername([Remainder]string username)
             {
-                User.UpdateUser(Context.User.Id, minecraftUsername:username);
+                //User.UpdateUser(Context.User.Id, minecraftUsername:username);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@minecraft", username)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET minecraftUsername=@minecraft WHERE id='" + Context.User.Id + "';", queryParams);
+
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the Minecraft username on your profile to \"" + username + "\"!");
             }
     
             [Command("snapchat"), Summary("")]
             public async Task SetSnapchatUsername([Remainder]string username)
             {
-                User.UpdateUser(Context.User.Id, snapchat:username);
+                //User.UpdateUser(Context.User.Id, snapchat:username);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@snapchat", username)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET snapchatUsername=@snapchat WHERE id='" + Context.User.Id + "';", queryParams);
+
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the Snapchat username on your profile to \"" + username + "\"!");
             }
     
             [Command("instagram"), Summary("")]
             public async Task SetInstagramUsername([Remainder]string username)
             {
-                User.UpdateUser(Context.User.Id, instagram:username);
+                //User.UpdateUser(Context.User.Id, instagram:username);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@instagram", username)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET instagramUsername=@instagram WHERE id='" + Context.User.Id + "';", queryParams);
+
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the Instagram username on your profile to \"" + username + "\"!");
             }
     
             [Command("github"), Summary("")]
             public async Task SetGitHubUsername([Remainder]string username)
             {
-                User.UpdateUser(Context.User.Id, github:username);
+                //User.UpdateUser(Context.User.Id, github:username);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@github", username)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET githubUsername=@github WHERE id='" + Context.User.Id + "';", queryParams);
+
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the GitHub username on your profile to \"" + username + "\"!");
             }
     
             [Command("websitename"), Summary("")]
             public async Task SetWebsiteName([Remainder]string name)
             {
-                User.UpdateUser(Context.User.Id, websiteName: name);
+                //User.UpdateUser(Context.User.Id, websiteName: name);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@websiteName", name)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET websiteName=@websiteName WHERE id='" + Context.User.Id + "';", queryParams);
+
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the website name on your profile to \"" + name + "\"!");
             }
     
             [Command("websiteurl"), Summary("")]
             public async Task SetWebsiteUrl([Remainder]string url)
             {
-                User.UpdateUser(Context.User.Id, websiteUrl: url);
+                //User.UpdateUser(Context.User.Id, websiteUrl: url);
+                
+                List<(string, string)> queryParams = new List<(string, string)>()
+                {
+                    ("@websiteUrl", url)
+                };
+                DatabaseActivity.ExecuteNonQueryCommand("UPDATE users SET websiteURL=@websiteUrl WHERE id='" + Context.User.Id + "';", queryParams);
+
                 await ReplyAsync(Context.User.Mention + ", you have successfully updated the website URL on your profile to \"" + url + "\"!");
             }
     
