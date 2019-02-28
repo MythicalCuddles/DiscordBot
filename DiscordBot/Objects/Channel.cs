68ccc2347d07ec51771e7bff8e422fd93ed8a377
Melissa <melissabrennan645@gmail.com>
2019-02-28 18:37:17 +0000

    Created the Channel Object to load information from the database

---
+++ DiscordBot/Objects/Channel.cs
@@ -0,0 +1,35 @@
+using DiscordBot.Database;
+using MySql.Data.MySqlClient;
+
+namespace DiscordBot.Objects
+{
+    public class Channel
+    {
+        public bool AwardingEXP = true;
+        
+        public Channel() { }
+
+        public Channel(bool? awardingEXP = null)
+        {
+            AwardingEXP = awardingEXP ?? AwardingEXP;
+        }
+        
+        public static Channel Load(ulong cId)
+        {
+            Channel channel = new Channel();
+                
+            (MySqlDataReader dr, MySqlConnection conn) reader = DatabaseActivity.ExecuteReader("SELECT * FROM channels WHERE channelID=" + cId + ";");
+            
+            while (reader.dr.Read())
+            {
+                if ((bool)reader.dr["awardingEXP"])
+                    channel.AwardingEXP = true;
+            }
+            
+            reader.dr.Close();
+            reader.conn.Close();
+            
+            return channel;
+        }
+    }
+}
