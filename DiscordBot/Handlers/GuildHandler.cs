52e172cdf8f086e545c56917a2073edff0e410c5
Melissa <melissabrennan645@gmail.com>
2019-02-28 15:58:05 +0000

    Added SQL Command to add channels to the database when the bot joins a new guild

--- DiscordBot/Handlers/GuildHandler.cs
+++ DiscordBot/Handlers/GuildHandler.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Discord;
 using Discord.WebSocket;
 
 using DiscordBot.Common;
+using DiscordBot.Database;
 using DiscordBot.Extensions;
 
 namespace DiscordBot.Handlers
@@ -16,7 +18,18 @@ namespace DiscordBot.Handlers
             GuildConfiguration.EnsureExists(socketGuild.Id);
 
             foreach (SocketGuildChannel c in socketGuild.Channels)
-                Channel.EnsureExists(c.Id);
+            {
+                List<(string, string)> queryParams = new List<(string id, string value)>()
+                {
+                    ("@channelName", c.Name),
+                    ("@channelType", c.GetType().Name)
+                };
+				    
+                int rowsUpdated = DatabaseActivity.ExecuteNonQueryCommand(
+                    "INSERT IGNORE INTO " +
+                    "channels(channelID,inGuildID,channelName,channelType) " +
+                    "VALUES (" + c.Id + ", " + c.Guild.Id + ", @channelName, @channelType);", queryParams);
+            }
 
             await Configuration.Load().LogChannelId.GetTextChannel().SendMessageAsync(socketGuild.Name + " has been added to " + DiscordBot.Bot.CurrentUser.Username + "'s guild list. \n" + socketGuild.Owner.Username + " is the owner (" + socketGuild.Owner.Id + ")");
             
