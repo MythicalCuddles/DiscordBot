341ee1bd78c8a5f0d3cd5f395536f8d1e4ca7c3d
Melissa <melissabrennan645@gmail.com>
2019-02-28 14:25:36 +0000

    Added an additional field into the channels table to identify the guild

--- DiscordBot/Database/DatabaseActivity.cs
+++ DiscordBot/Database/DatabaseActivity.cs
@@ -262,6 +262,7 @@ namespace DiscordBot.Database
             
             ExecuteNonQueryCommand("CREATE TABLE IF NOT EXISTS `channels` (" +
                                    "`channelID` bigint(20) NOT NULL," +
+                                   "`inGuildID` bigint(20) NOT NULL," +
                                    "`channelName` text COLLATE utf8mb4_unicode_ci NOT NULL," +
                                    "`awardingEXP` tinyint(1) NOT NULL DEFAULT '1'," +
                                    "PRIMARY KEY (`channelID`)" +
