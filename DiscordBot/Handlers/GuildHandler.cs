88864c5355d9184e162cc1a620b0a4e046d87b35
Melissa <melissabrennan645@gmail.com>
2019-03-01 18:36:15 +0000

    Added InsertGuildToDB to issue an MySQL Command to the database inserting the guild passed Added additional guild event methods

--- DiscordBot/Handlers/GuildHandler.cs
+++ DiscordBot/Handlers/GuildHandler.cs
@@ -15,26 +15,52 @@ namespace DiscordBot.Handlers
     {
         public static async Task JoinedGuild(SocketGuild socketGuild)
         {
-            GuildConfiguration.EnsureExists(socketGuild.Id);
-
+            await InsertGuildToDB(socketGuild);
+            
             foreach (SocketGuildChannel c in socketGuild.Channels)
             {
-                List<(string, string)> queryParams = new List<(string id, string value)>()
-                {
-                    ("@channelName", c.Name),
-                    ("@channelType", c.GetType().Name)
-                };
-				    
-                int rowsUpdated = DatabaseActivity.ExecuteNonQueryCommand(
-                    "INSERT IGNORE INTO " +
-                    "channels(channelID,inGuildID,channelName,channelType) " +
-                    "VALUES (" + c.Id + ", " + c.Guild.Id + ", @channelName, @channelType);", queryParams);
+                await ChannelHandler.InsertChannelToDB(c);
             }
 
             await Configuration.Load().LogChannelId.GetTextChannel().SendMessageAsync(socketGuild.Name + " has been added to " + DiscordBot.Bot.CurrentUser.Username + "'s guild list. \n" + socketGuild.Owner.Username + " is the owner (" + socketGuild.Owner.Id + ")");
             
             await new LogMessage(LogSeverity.Info, "JoinedGuild", "[" + socketGuild.Name + "] " + "[@" + socketGuild.Owner.Username + 
                                                               "] : Bot Joined the Guild").PrintToConsole();
+        }
+
+        public static async Task InsertGuildToDB(SocketGuild g)
+        {
+            SocketGuildUser gBot = g.GetUser(DiscordBot.Bot.CurrentUser.Id);
+            
+            List<(string, string)> queryParams = new List<(string id, string value)>()
+            {
+                ("@guildID", g.Id.ToString()),
+                ("@guildName", g.Name),
+                ("@guildIcon", g.IconUrl),
+                ("@ownedBy", g.Owner.Id.ToString()),
+                ("@dateJoined", gBot.JoinedAt.Value.DateTime.ToString("u")), //"yyyy-MM-dd HH:mm:ss"
+                ("@guildPrefix", "$") 
+            };
+				    
+            DatabaseActivity.ExecuteNonQueryCommand(
+                "INSERT IGNORE INTO " +
+                "guilds(guildID,guildName,guildIcon,ownedBy,dateJoined,guildPrefix) " +
+                "VALUES (@guildID, @guildName, @guildIcon, @ownedBy, @dateJoined, @guildPrefix);", queryParams);
+        }
+
+        public static async Task LeftGuild(SocketGuild socketGuild)
+        {
+            
+        }
+
+        public static async Task GuildUpdated(SocketGuild cachedSocketGuild, SocketGuild socketGuild)
+        {
+            
+        }
+
+        public static async Task GuildMemberUpdated(SocketGuildUser cachedSocketGuildUser, SocketGuildUser socketGuildUser)
+        {
+            
         }
     }
 }
