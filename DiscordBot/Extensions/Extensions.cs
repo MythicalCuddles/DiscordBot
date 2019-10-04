3593d2eaa58602375b4ab74ca8f0627e53e8c58c
Melissa <melissabrennan645@gmail.com>
2019-10-04 18:27:07 +0100

    Added PrintLogMessage() to Extensions to print a formatted message to the console

--- DiscordBot/Extensions/Extensions.cs
+++ DiscordBot/Extensions/Extensions.cs
@@ -402,5 +402,11 @@ namespace DiscordBot.Extensions
         {
             await DiscordBot.Log(logMessage);
         }
+        
+        public static async void PrintLogMessage(this string source, string message, LogSeverity logSeverity = LogSeverity.Info)
+        {
+            var logMessage = new LogMessage(logSeverity, source, message);
+            await logMessage.PrintToConsole();
+        }
     }
 }
