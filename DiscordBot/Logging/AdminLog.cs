66badbb5487a0600b8921d2a8b50610b3e102be7
Melissa <melissabrennan645@gmail.com>
2019-10-18 19:17:57 +0100

    Cleaned up AdminLog

--- DiscordBot/Logging/AdminLog.cs
+++ DiscordBot/Logging/AdminLog.cs
@@ -1,15 +1,13 @@
-using System;
 using System.Collections.Generic;
 using DiscordBot.Database;
-using MySql.Data.MySqlClient;
 
 namespace DiscordBot.Logging
 {
     public class AdminLog
     {
-        internal static bool Log(ulong executedBy, string action, ulong executedIn, ulong? userMention = null)
+        internal static void Log(ulong executedBy, string action, ulong executedIn, ulong? userMention = null)
         {
-            List<(string, string)> queryParams = new List<(string id, string value)>()
+            List<(string, string)> queryParams = new List<(string id, string value)>
             {
                 ("@executedBy", executedBy.ToString()),
                 ("@action", action),
@@ -20,12 +18,10 @@ namespace DiscordBot.Logging
                 ? ("@mentionedUser", userMention.ToString())
                 : ("@mentionedUser", null));
 
-            int rowsUpdated = DatabaseActivity.ExecuteNonQueryCommand(
+            DatabaseActivity.ExecuteNonQueryCommand(
                 "INSERT IGNORE INTO " +
                 "admin_log(logId,executedBy,action,executedAt,executedIn,userMentioned) " +
                 "VALUES (NULL, @executedBy, @action, CURRENT_TIMESTAMP, @executedIn, @mentionedUser);", queryParams);
-            
-            return rowsUpdated == 1;
         }
     }
 }
