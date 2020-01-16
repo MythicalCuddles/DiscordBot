bc5767c7bdac76e7bf0306edf889d016bc02c449
Melissa <melissabrennan645@gmail.com>
2020-01-16 10:37:13 +0000

    Added LoadAll to Award to load all the awards from the database on startup

--- DiscordBot/Objects/Award.cs
+++ DiscordBot/Objects/Award.cs
@@ -1,24 +1,29 @@
 using System;
+using System.Collections.Generic;
+using DiscordBot.Database;
+using MySql.Data.MySqlClient;
 
 namespace DiscordBot.Objects
 {
     public class Award
     {
-        private ulong _awardId;
+        private int _awardId;
         private ulong _userId;
-        private string _awardText;
+        private string _awardText;//, _awardType;
         private DateTime _dateAwarded;
+        
+        internal static List<Award> Awards = new List<Award>();
 
         public Award() { }
 
-        public Award(ulong awardId, ulong userId, string awardText)
+        public Award(int awardId, ulong userId, string awardText)
         {
             _awardId = awardId;
             _userId = userId;
             _awardText = awardText;
         }
         
-        public Award(ulong awardId, ulong userId, string awardText, DateTime dateAwarded)
+        public Award(int awardId, ulong userId, string awardText, DateTime dateAwarded)
         {
             _awardId = awardId;
             _userId = userId;
@@ -26,7 +31,7 @@ namespace DiscordBot.Objects
             _dateAwarded = dateAwarded;
         }
 
-        public ulong AwardId
+        public int AwardId
         {
             get => _awardId;
             set => _awardId = value;
@@ -48,6 +53,31 @@ namespace DiscordBot.Objects
         {
             get => _dateAwarded;
             set => _dateAwarded = value;
+        }
+
+        internal static List<Award> LoadAll()
+        {
+            List<Award> awards = new List<Award>();
+
+            (MySqlDataReader dr, MySqlConnection conn) reader = DatabaseActivity.ExecuteReader("SELECT * FROM awards;");
+
+            while (reader.dr.Read())
+            {
+                var a = new Award
+                {
+                    _awardId = reader.dr.GetInt32("awardID"),
+                    _userId = reader.dr.GetUInt64("userID"),
+                    _awardText = reader.dr.GetString("awardText"),
+                    _dateAwarded = reader.dr.GetDateTime("dateAwarded")
+                };
+                
+                awards.Add(a);
+            }
+            
+            reader.dr.Close();
+            reader.conn.Close();
+
+            return awards;
         }
     }
 }
